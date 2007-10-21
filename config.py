b4fcac506d5bf8ecc5e5c3c7ca2e788e7ef1b128
Fede <fedux@linux-kt9u.site>
2007-10-21 18:42:14 -0300

    Mod de los parametros de inicializacion de vrrp.

--- config.py
+++ config.py
@@ -13,25 +13,33 @@ class Root(Handler):
     ip = IpHandler(
         pickle_dir = join(pickle_path, 'ip'),
         config_dir = join(config_path, 'ip'))
+
+    firewall = FirewallHandler(
+        pickle_dir = join(pickle_path, 'firewall'),
+        config_dir = '/tmp')
+
     dhcp = DhcpHandler(
         pickle_dir = join(pickle_path, 'dhcp'),
         config_dir = '/etc')
+
     dns = DnsHandler(
         pickle_dir = join(pickle_path, 'dns'),
         config_dir = {
             'named.conf': '/etc',
             'zoneX.zone': '/var/lib/named',
         })
-    firewall = FirewallHandler(
-        pickle_dir = join(pickle_path, 'firewall'),
-        config_dir = '/tmp')
+
     nat = NatHandler(pickle_dir = join(pickle_path, 'nat'))
+
     proxy = ProxyHandler(
         pickle_dir = join(pickle_path, 'proxy'),
-        config_dir = '/etc/squid'))
+        config_dir = '/etc/squid')
+
     vrrp = VrrpHandler(
         pickle_dir = join(pickle_path, 'vrrp'),
-        config_dir = join(config_path, 'vrrp'))
+        config_dir = join(config_path, 'vrrp'),
+        pid_dir    = '/var/run')
+
     ppp = PppHandler(
         pickle_dir = join(pickle_path, 'ppp'),
         config_dir = {
