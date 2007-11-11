7c5adb595e720d430734532b5f3c9a337198a047
Leandro Lucarella <llucax@gmail.com>
2007-11-11 17:00:41 -0300

    Bugfix: InitdHandler.timer_handler() had undefined variables.

--- pymin/services/util.py
+++ pymin/services/util.py
@@ -643,10 +643,10 @@ class InitdHandler(ServiceHandler):
         p = subprocess.Popen(('pgrep', '-f', self._initd_name),
                                 stdout=subprocess.PIPE)
         pid = p.communicate()[0]
-        if p.wait() == 0 and len(pid) > 0:
-            c._service_running = True
+        if p.returncode == 0 and len(pid) > 0:
+            self._service_running = True
         else:
-            c._service_running = False
+            self._service_running = False
 
 class TransactionalHandler(Handler):
     r"""Handle command transactions providing a commit and rollback commands.
