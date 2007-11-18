07a7ae61f3af4fa685077e3b7e5c59f09f8a143f
Leandro Lucarella <llucax@gmail.com>
2007-11-18 02:51:10 -0300

    Add logging to pymin.services.util module.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 import logging
 logging.basicConfig(
         level   = logging.DEBUG,
-        format  = '%(asctime)s %(name)-18s %(levelname)-8s %(message)s',
+        format  = '%(asctime)s %(name)-24s %(levelname)-8s %(message)s',
         datefmt = '%a, %d %b %Y %H:%M:%S',
 )
 
